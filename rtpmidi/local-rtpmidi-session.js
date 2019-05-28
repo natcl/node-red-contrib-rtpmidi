9d5cb74b4e91c8d1521013706505e3bf31877e51
Renaud Vincent <renaudv@momentfactory.com>
2019-05-28 15:47:35 -0400

    Hangling session reset on deploy

--- rtpmidi/local-rtpmidi-session.js
+++ rtpmidi/local-rtpmidi-session.js
@@ -1,3 +1,5 @@
+const rtpmidi = require('rtpmidi');
+
 module.exports = function(RED) {
 
   function LocalRTPMIDISession(n) {
@@ -5,6 +7,19 @@ module.exports = function(RED) {
     this.localName = n.localName;
     this.bonjourName = n.bonjourName;
     this.port = n.port;
+
+    this._session = rtpmidi.manager.createSession({
+      localName: this.localName,
+      bonjourName: this.bonjourName,
+      port: parseInt(this.port) // When sent from UI, parsed as string
+    });
+
+    this.on('close', function(done) {
+      rtpmidi.manager.reset(function() {
+        // When all sessions closed, proceed
+        done();
+      });
+    });
   }
 
   RED.nodes.registerType("local-rtpmidi-session", LocalRTPMIDISession);
--- rtpmidi/rtpmidi.js
+++ rtpmidi/rtpmidi.js
@@ -23,24 +23,19 @@ module.exports = function(RED) {
       const local = RED.nodes.getNode(config.local);
       const remote = RED.nodes.getNode(config.remote);
 
-      // Prevent error if node-red failed to reset session
-      if(!rtpmidi.manager.getSessionByPort(local.port))
-        node._session = rtpmidi.manager.createSession({
-          localName: local.localName,
-          bonjourName: local.bonjourName,
-          port: parseInt(local.port) // When sent from UI, parsed as string
-        });
+      node._session = local._session;
 
       node._mtc = new rtpmidi.MTC();
       node._mtc.setSource(node._session);
+
       node._remote = {
         address: remote.host,
         port: parseInt(remote.port) // When sent from UI, parsed as string
       }
 
-      node._session.connect(node._remote);
 
       node._session.on('ready', function() {
+        node._session.connect(node._remote);
         node.status({ fill:"green", shape:"dot", text:"ready"});
       });
 
@@ -48,12 +43,15 @@ module.exports = function(RED) {
         // For compliance with the node-red-contrib-midi package
         const msg = {};
         msg.midi = {};
-        msg.midi.raw = message.slice();
-        msg.payload = message.splice(1);
+
+        if(!!message) {
+          msg.midi.raw = message.slice();
+          msg.payload = message.splice(1);
+          msg.midi.channel = (message & 0xF) + 1;
+          msg.midi.type = midiTypes[message >> 4];
+        }
 
         msg.midi.deltaTime = deltaTime;
-        msg.midi.channel = (message & 0xF) + 1;
-        msg.midi.type = midiTypes[message >> 4];
         msg.midi.data = msg.payload;
 
         node.send(msg);
@@ -62,13 +60,6 @@ module.exports = function(RED) {
       node._mtc.on('change', function() {
         // Log the time code HH:MM:SS:FF
         node.send({payload: {position: node._mtc.songPosition, time: node._mtc.getSMTPEString()}});
-      });
-
-      node.on('close', function(done) {
-        rtpmidi.manager.reset(function() {
-          // When all sessions closed, proceed
-          done();
-        });
       });
 
     } catch (error) {
