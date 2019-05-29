cf43b12faf5e9b24fc342dc2d8e1ff189fac98df
Renaud Vincent <renaudv@momentfactory.com>
2019-05-29 13:03:38 -0400

    Some error mgmt, error on 2 nodes and delete --> socket issue

--- rtpmidi/local-rtpmidi-session.js
+++ rtpmidi/local-rtpmidi-session.js
@@ -8,18 +8,43 @@ module.exports = function(RED) {
     this.bonjourName = n.bonjourName;
     this.port = n.port;
 
-    this._session = rtpmidi.manager.createSession({
-      localName: this.localName,
-      bonjourName: this.bonjourName,
-      port: parseInt(this.port) // When sent from UI, parsed as string
-    });
+    try {
 
-    this.on('close', function(done) {
-      rtpmidi.manager.reset(function() {
-        // When all sessions closed, proceed
-        done();
+      this._session = rtpmidi.manager.createSession({
+        localName: this.localName,
+        bonjourName: this.bonjourName,
+        port: parseInt(this.port) // When sent from UI, parsed as string
       });
-    });
+
+      this._session.on('error', function(err) {
+        console.error(err);
+      });
+
+
+      var node = this;
+      this.on('close', function(done) {
+        rtpmidi.manager.reset(function() {
+         /*  // Close everything properly
+          console.log('THERE SHOULD BE NO SESSIONS NOW', rtpmidi.manager.getSessions())
+          const sessions = rtpmidi.manager.getSessions();
+          function next() {
+            if(rtpmidi.manager.getSessions().length != 0) {
+              console.log('Still not cleared')
+
+              next();
+            } else {
+              done();
+            }
+          }
+          next(); */
+          done();
+        });
+      });
+    } catch (error) {
+      console.log(error);
+      this.status({ fill:"red", shape:"dot", text: "error"});
+    }
+
   }
 
   RED.nodes.registerType("local-rtpmidi-session", LocalRTPMIDISession);
--- rtpmidi/rtpmidi.js
+++ rtpmidi/rtpmidi.js
@@ -13,33 +13,33 @@ var midiTypes = {
 module.exports = function(RED) {
   function RTPMidiMTCInNode(config) {
     RED.nodes.createNode(this, config);
-    var node = this;
 
     try {
-
       if(!config.local) throw new Error("ERROR: Missing local session config node");
       if(!config.remote) throw new Error("ERROR: Missing local session config node");
 
       const local = RED.nodes.getNode(config.local);
       const remote = RED.nodes.getNode(config.remote);
 
-      node._session = local._session;
+      this._session = local._session;
 
-      node._mtc = new rtpmidi.MTC();
-      node._mtc.setSource(node._session);
+      this._mtc = new rtpmidi.MTC();
+      this._mtc.setSource(this._session);
 
-      node._remote = {
+      this._remote = {
         address: remote.host,
         port: parseInt(remote.port) // When sent from UI, parsed as string
-      }
+      };
 
+      // To use in sub-callbacks
+      var node = this;
 
-      node._session.on('ready', function() {
+      this._session.on('ready', function() {
         node._session.connect(node._remote);
         node.status({ fill:"green", shape:"dot", text:"ready"});
       });
 
-      node._session.on('message', function(deltaTime, message) {
+      this._session.on('message', function(deltaTime, message) {
         // For compliance with the node-red-contrib-midi package
         const msg = {};
         msg.midi = {};
@@ -57,14 +57,20 @@ module.exports = function(RED) {
         node.send(msg);
       });
 
-      node._mtc.on('change', function() {
+      this._mtc.on('change', function() {
         // Log the time code HH:MM:SS:FF
         node.send({payload: {position: node._mtc.songPosition, time: node._mtc.getSMTPEString()}});
       });
 
+      // Close all sessions, including remote ones
+      this.on('close', function(done) {
+        node._session = undefined;
+        done();
+      });
+
     } catch (error) {
-      node._err = error.message;
-      node.status({ fill:"red", shape:"dot", text: "error"});
+      this._err = error.message;
+      this.status({ fill:"red", shape:"dot", text: "error"});
     }
   }
 
